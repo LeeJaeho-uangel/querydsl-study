8e62c4bf351d357f789a62527c5f783d231002e9
LeeJaeho-uangel <109058571+LeeJaeho-uangel@users.noreply.github.com>
2023-01-18 11:18:50 +0900

    프로젝트 생성

---
+++ src/main/java/study/querydsl/entity/Hello.java
@@ -0,0 +1,20 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Hello {
+
+  @Id
+  @GeneratedValue
+  private long id;
+}
---
+++ src/test/java/study/querydsl/entity/HelloTest.java
@@ -0,0 +1,38 @@
+package study.querydsl.entity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+class HelloTest {
+
+  @Autowired
+  EntityManager em;
+
+  JPAQueryFactory queryFactory;
+
+  @BeforeEach
+  public void before() {
+    queryFactory = new JPAQueryFactory(em);
+  }
+
+  @Test
+  public void helloTest() {
+    Hello hello = new Hello();
+    em.persist(hello);
+
+    QHello q = new QHello("q");
+    Hello result = queryFactory
+        .selectFrom(q)
+        .fetchOne();
+
+    System.out.println("result = " + result);
+  }
+}
